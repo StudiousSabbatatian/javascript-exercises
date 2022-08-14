4197ec14ec9abcfccddf8fbdf1618f4d93d45c72
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-14 09:20:40 -0500

    Complete 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rest) {
+    if (Array.isArray(array) === false) {
+        throw "First argument is not an array!"
+    }
+    rest.forEach(function(element) {
+        let index = array.indexOf(element)
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
 
+    })
+    return array;
 };
 
 // Do not edit below this line

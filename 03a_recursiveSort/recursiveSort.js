e965ac3214e47cd75e6341ac3d42289724d8d15c
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-14 08:12:53 -0500

    Implement sorting algorithm for recursiveSort

--- 03a_recursiveSort/recursiveSort.js
+++ 03a_recursiveSort/recursiveSort.js
@@ -7,12 +7,19 @@ const recursiveSort = function(array) {
         return array;
     }
     let leftSideCutoff = parseInt(arrLen / 2);
-    console.log(leftSideCutoff)
+    //console.log(leftSideCutoff)
 //sort left side
+    let left = recursiveSort(array.slice(1, leftSideCutoff + 1))
 //sort right side
+    let right = recursiveSort(array.slice(leftSideCutoff + 1, arrLen + 1))
 //swap if needed
-
+    if (left[left.length - 1] > right[0]) {
+        return concat(right, left);
+    } else {
+        return concat(left, right);
+    }
 };
 // Do not edit below this line
 module.exports = recursiveSort;
-recursiveSort(array);
+let arr = recursiveSort(array);
+console.log(arr);

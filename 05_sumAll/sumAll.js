2c623b29388bddf0df68eaab4f8560900170e250
Luc Mainguy <luc.mainguy1@gmail.com>
2022-08-14 09:39:58 -0500

    Complete 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a > b) {
+        var big = a, small = b;
+    } else {
+        var big = b, small = a;
+    }
+    let sum = 0;
+    for (i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 

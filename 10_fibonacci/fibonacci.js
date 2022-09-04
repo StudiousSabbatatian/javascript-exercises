04257219753a1b7896f04a37da3117f9288f2ffb
Luc Mainguy <luc.mainguy1@gmail.com>
2022-09-03 22:27:05 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (typeof index === 'string') {
+        index = parseInt(index);
+    }
+    if (index < 0 || index === NaN) {
+        return "OOPS"
+    }
+    if (index === 1 || index === 2) {
+        return 1;
+    }
+    let firstPrevious = 1;
+    let secondPrevious = 1;
+    for (i = 3; i <= index; i++) {
+        current = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = current;
+    }
+    return current;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;

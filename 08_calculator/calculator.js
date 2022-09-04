91427cd473cdf95dfa5d74a2ce78d8b3cd291bc4
Luc Mainguy <luc.mainguy1@gmail.com>
2022-09-03 21:47:15 -0500

    Complete 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((runningtotal, current) => runningtotal + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((runningproduct, current) => runningproduct * current, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let returnVal = 1;
+  for (i = number; i >= 1; i--) {
+    returnVal = returnVal * i;
+  }
+  return returnVal
 };
 
 // Do not edit below this line

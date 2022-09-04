d6a7ef3f410a5f47a01ab03c3adf47dffd45170f
Luc Mainguy <luc.mainguy1@gmail.com>
2022-09-03 22:13:31 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    const arr  = string.split("");
+    let arrMiddle = arr.length / 2;
+    let firstHalf = arr.slice(0, Math.floor(arrMiddle));
+    let secondHalf = arr.slice(Math.ceil(arrMiddle));
+    secondHalf = secondHalf.reverse();
+    return firstHalf.length === secondHalf.length &&
+    firstHalf.every((val, index) => val === secondHalf[index]);
 };
 
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 // Do not edit below this line
 module.exports = palindromes;
